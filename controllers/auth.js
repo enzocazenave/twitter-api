1ff28b59d21afa6c79052c13eedcafbe5408e772
Enzo <encaze24@hotmail.com>
2022-10-07 12:59:32 -0300

    Add jwt verify

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 const registerUser = async(req, res = response) => {
@@ -32,12 +33,78 @@ const registerUser = async(req, res = response) => {
 
         await user.save();
 
-        res.status(201).json({
-            ok: true,
-            uid: user.id,
+        const data = {
+            id: user.id,
             name: user.name,
-            username: user.username
+            username: user.username,
+            email: user.email,
+            birthdate: user.birthdate
+        }
+
+        jwt.sign(data, process.env.SECRET_JWT_KEY, { expiresIn: '1h' }, (err, token) => {
+            if (err) return res.status(400).json({
+                ok: false,
+                msg: 'Failed to generate token'
+            });
+
+            res.status(201).json({
+                ok: true,
+                ...data,
+                token
+            });
+        });
+    } catch(error) {
+        console.log(error);
+
+        res.status(500).json({
+            ok: false,
+            msg: '[ERROR] Please talk with the administrator'
         })
+    }
+}
+
+const loginUser = async(req, res = response) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Incorrect credentials'
+            });
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+        if(!isPasswordValid) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Incorrect credentials'
+            });
+        }
+
+        const data = {
+            id: user.id,
+            name: user.name,
+            username: user.username,
+            email: user.email,
+            birthdate: user.birthdate
+        }
+
+        jwt.sign(data, process.env.SECRET_JWT_KEY, { expiresIn: '1h' }, (err, token) => {
+            if (err) return res.status(400).json({
+                ok: false,
+                msg: 'Failed to generate token'
+            });
+
+            res.status(201).json({
+                ok: true,
+                ...data,
+                token
+            });
+        });
     } catch(error) {
         console.log(error);
 
@@ -50,4 +117,5 @@ const registerUser = async(req, res = response) => {
 
 module.exports = {
     registerUser,
+    loginUser
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { isDate } = require('../helpers/isDate');
-const { registerUser } = require('../controllers/auth');
+const { registerUser, loginUser } = require('../controllers/auth');
 const { fieldValidator } = require('../middlewares/field-validators');
 
 const router = Router();
@@ -10,12 +9,21 @@ router.post('/register',
     [
         check('name', 'Your name is required').not().isEmpty(),
         check('username', 'Your username is required').not().isEmpty(),
-        check('birthdate', 'Your birthdate is invalid').custom(isDate),
+        check('birthdate', 'Your birthdate is invalid').not().isEmpty(),
         check('email', 'Your email is required').isEmail(),
         check('password', 'Password must be 6 characters or more').isLength({ min: 6 }),
         fieldValidator
     ],
     registerUser
+);
+
+router.post('/login',
+    [
+        check('email', 'Your email is required').isEmail(),
+        check('password', 'Password must be 6 characters or more').isLength({ min: 6 }),
+        fieldValidator
+    ],
+    loginUser
 );
 
 module.exports = router;
