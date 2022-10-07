5bc8f28ab2631aed6404f27e8803aada9a06cfe9
Enzo <encaze24@hotmail.com>
2022-10-06 23:53:54 -0300

    Connect with Mongo DB

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const { dbConnection } = require('./database/config');
 
 const app = express();
+
+dbConnection();
 
 app.use(cors());
 app.use(express.static('public'));
@@ -11,5 +14,5 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on: http://localhost:${ process.env.PORT }`);
+    console.log(`\nSTARTING SERVER\n✔ http://localhost:${ process.env.PORT }`);
 });
